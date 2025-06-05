fdd382bfb62fb1efc6deb6c891a7f76492bafab9
Bloodshot515 <akumaniku52@gmail.com>
2025-06-05 00:38:56 -0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,23 +1,16 @@
+
 import telebot
 import os
 
-TOKEN = os.getenv("BOT_TOKEN")
+TOKEN = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN")
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Привет! Я готов работать.")
+    bot.send_message(message.chat.id, "Привет! Бот работает корректно.")
 
-@bot.message_handler(commands=['погода'])
-def weather(message):
-    bot.reply_to(message, "Сегодня солнечно, +25°C")
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.send_message(message.chat.id, "Вы написали: " + message.text)
 
-@bot.message_handler(commands=['топливо'])
-def fuel(message):
-    bot.reply_to(message, "Средняя цена дизеля: $3.78")
-
-@bot.message_handler(commands=['ставка'])
-def rate(message):
-    bot.reply_to(message, "Ставка по региону: $0.92/миля")
-
-bot.polling()
+bot.polling(non_stop=True)
